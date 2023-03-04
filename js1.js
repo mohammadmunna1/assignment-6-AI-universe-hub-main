26b73231d22fe8bb2f85f5873f3635410f515231
mohammadmunna13 <mohammadmuna4141@gmail.com>
2023-03-04 22:27:04 +0600

    Sorting by date

--- js1.js
+++ js1.js
@@ -10,9 +10,33 @@ const loadAiInformation = (dataLimit) => {
         .catch(error => console.log(error));
 }
 
-
+            //Display fetched data
 
 const displayAiInformation = (info, dataLimit, sortDataByDate) => {
 
     const seeMoreButton = document.getElementById('see-more-button');
     const cardContainer = document.getElementById('card-container');
+
+
+           //Restricting data
+
+    if (info.length > 6 && dataLimit) {
+        info = info.slice(0, 6);
+    }
+    else {
+        seeMoreButton.classList.add('d-none');
+    }  
+
+    document.getElementById('date-button').addEventListener('click', async () => {;
+        const sortedData = info.sort((a, b) => new Date(a.published_in) - new Date(b.published_in));
+        toggleSpinner(true);
+        const cardContainer = document.getElementById('card-container');
+        cardContainer.textContent = '';
+
+        //Fetching each data sorting by date 
+        info.forEach(sortedData => {
+        processData(sortedData);
+        toggleSpinner(false);
+        return;
+        });
+    });
